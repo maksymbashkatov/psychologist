014a2fc8eea4fd39736208206fb4c5a5bcb2a99f
maksymbashkatov <maksymbashkatov@gmail.com>
2025-05-13 18:26:37 +0300

    feat(header): add menu for mobile

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import Link from 'next/link';
 import styles from './header.module.css';
 import { menu } from '../data';
@@ -5,9 +7,14 @@ import Telegram from './social-icons/telegram';
 import WhatsApp from './social-icons/whatsapp';
 import Youtube from './social-icons/youtube';
 import Instagram from './social-icons/instagram';
+import { useState } from 'react';
 
 export default function Header(
 ) {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
+  
   return <header>
     <div className={`${styles.header} container`}>
       <Link className={styles.logo} href='/'>Диана Лонская</Link>
@@ -25,6 +32,25 @@ export default function Header(
         <Youtube fill={'#DFBFC0'}/>
         <Instagram fill={'#DFBFC0'}/>
       </div>
+      <div className={styles.mobMenuButton} onClick={toggleMenu}>
+        {
+          [1, 2, 3].map((e) => <div
+            key={e}
+            className={`${styles.mobMenuLine} ${isMenuOpen ? styles.mobMenuLineOpen: ''}`}
+          >
+          </div>)
+        }
+      </div>
+      <div className={`${styles.mobMenu} ${styles.mobMenu1} ${isMenuOpen ? styles.mobMenuOpen : ''}`}></div>
+      <nav className={`${styles.mobMenu} ${styles.mobMenu2} ${isMenuOpen ? styles.mobMenuOpen : ''}`}>
+        {
+          menu.map((m) =>
+          <Link className={styles.menuItem} href={m.link} key={m.id} onClick={toggleMenu}>
+            {m.text}
+          </Link>)
+        }
+      </nav>
+      <div className={`${styles.mobMenu} ${styles.mobMenu3} ${isMenuOpen ? styles.mobMenuOpen : ''}`}></div>
     </div>
   </header>
 }
