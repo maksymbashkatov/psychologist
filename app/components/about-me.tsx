61a6ef344df4d3fc81be4912baf597c8b733f7b2
maksymbashkatov <maksymbashkatov@gmail.com>
2025-05-14 22:47:58 +0300

    feat: add aboutMe for two desktop versions

---
+++ app/components/about-me.tsx
@@ -0,0 +1,33 @@
+import Link from 'next/link';
+import styles from './about-me.module.css';
+
+const theses = [
+  {id: 1, content: 'Практикующий психолог'},
+  {id: 2, content: 'Сертифицированный экзистенциальный и психоаналитический психотерапевт'},
+  {id: 3, content: 'Опыт работы более 13 лет'}
+]
+
+export default function AboutMe() {
+  return <section className={`${styles.aboutMe} section`}>
+    <div className='container'>
+      <div className={styles.content}>
+        <h2 className={styles.title}>Обо мне</h2>
+        <div className={styles.theses}>
+          {
+            theses.map((t) => <div
+              key={t.id}
+              className={styles.thesis}
+            >
+              <div></div>
+              <p>{t.content}</p>
+            </div>)
+          }
+        </div>
+        <p className={styles.quote}>“Помогаю исследовать внутренние тупики, разбираться с чувствами, осознавать конфликты, искать смыслы и осуществлять выбор.”</p>
+        <Link href='/obo-mne' className={styles.button}>
+          <p>Подробнее</p>
+        </Link>
+      </div>
+    </div>
+  </section>
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import AboutMe from "./components/about-me";
 import Hero from "./components/hero";
 
 export default function Page() {
   return <main>
     <Hero/>
+    <AboutMe/>
   </main>;
 }
