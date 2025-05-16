c7363d464e27ded3723511d1ab542a56a1084318
maksymbashkatov <maksymbashkatov@gmail.com>
2025-05-16 16:56:15 +0300

    feat: add services for two desktop versions

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import AboutMe from "./components/about-me";
 import Hero from "./components/hero";
+import Services from "./components/services";
 
 export default function Page() {
   return <main>
     <Hero/>
     <AboutMe/>
+    <Services/>
   </main>;
 }
