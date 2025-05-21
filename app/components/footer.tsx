90d894676654eaa4c38bcb354f5829a66ce99e4b
maksymbashkatov <maksymbashkatov@gmail.com>
2025-05-21 21:00:35 +0300

    feat: add footer for two desktop versions and for tablet & mobile

---
+++ app/components/footer.tsx
@@ -0,0 +1,45 @@
+import styles from './footer.module.css';
+import { menu } from '../data';
+import Link from 'next/link';
+import Telegram from './social-icons/telegram';
+import WhatsApp from './social-icons/whatsapp';
+import Youtube from './social-icons/youtube';
+import Instagram from './social-icons/instagram';
+
+export default function Footer() {
+  return <footer className='section'>
+    <div className={`${styles.container} container`}>
+      <div className={styles.contacts}>
+        <h4>Диана Лонская</h4>
+        <p>dialon.psy@gmail.com</p>
+        <div className={styles.socials}>
+          <Telegram fill={'#4C5866'}/>
+          <WhatsApp fill={'#A6ABB3'}/>
+          <Youtube fill={'#A6ABB3'}/>
+          <Instagram fill={'#A6ABB3'}/>
+        </div>
+      </div>
+      <div className={styles.menu}>
+        <h4>Меню</h4>
+        {
+          menu.map((m) => <Link
+            key={m.id}
+            href={m.link}
+          >
+            {m.text}
+          </Link>)
+        }
+      </div>
+      <div className={styles.details}>
+        <h4>Реквизиты</h4>
+        <p>Индивидуальный предприниматель</p>
+        <p>Лонская Диана Владимировна</p>
+        <p>ИНН: 7777777777</p>
+      </div>
+      <div className={styles.privacy}>
+        <Link href={'/'}>Политика конфиденцеальности</Link>
+        <Link href={'/'}>Оферта</Link>
+      </div>
+    </div>
+  </footer>
+}
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata, Viewport } from 'next';
 import './globals.css';
 import Header from './components/header';
+import Footer from './components/footer';
 
 export const metadata: Metadata = {
   title: '',
@@ -30,6 +31,7 @@ export default function RootLayout({
     <body>
       <Header/>
       {children}
+      <Footer/>
     </body>
   </html>;
 }
