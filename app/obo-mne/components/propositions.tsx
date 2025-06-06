d77b02a310e64d005b636487675cd78804d39bc3
maksymbashkatov <maksymbashkatov@gmail.com>
2025-06-06 22:05:53 +0300

    feat: add propositions block for all ekrans

---
+++ app/obo-mne/components/propositions.tsx
@@ -0,0 +1,23 @@
+import styles from './propositions.module.css';
+
+export default function Propositions() {
+  return <section className={`${styles.propositions} container`}>
+    <div className={styles.proposition}>
+      <img src='/propositions/1.webp'></img>
+      <h3>Индивидуальные консультации очно и онлайн</h3>
+    </div>
+    <div className={styles.proposition}>
+      <img src='/propositions/2.webp'></img>
+      <h3>Мастер-классы и обучение</h3>
+    </div>
+    <div className={styles.proposition}>
+      <img src='/propositions/3.webp'></img>
+      <h3>Длительная терапия</h3>
+    </div>
+    <div className={styles.proposition}>
+      <img src='/propositions/4.webp'></img>
+      <h3>Супервизия</h3>
+    </div>
+    <p>Это позволит вам изменить привычный взгляд на себя, увидеть скрытые мотивы ваших выборов и привнесет в вашу жизнь больше осмысленности и удовлетворенности</p>
+  </section>
+}
--- app/obo-mne/page.tsx
+++ app/obo-mne/page.tsx
@@ -1,7 +1,9 @@
 import Hero from './components/hero';
+import Propositions from './components/propositions';
 
 export default function Page() {
   return <main>
     <Hero/>
+    <Propositions/>
   </main>;
 }
