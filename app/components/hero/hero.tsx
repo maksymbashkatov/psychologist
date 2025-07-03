f3f447f352158fec89fd9fbe975c5c06027f48fa
maksymbashkatov <maksymbashkatov@gmail.com>
2025-07-03 16:54:13 +0300

    refactor: correct hero block (all ekrans)

--- app/components/hero/hero.tsx
+++ app/components/hero/hero.tsx
@@ -9,11 +9,10 @@ export default function Hero(
   return <div className={`${styles.heroContainer} container`}>
       <div className={styles.heroContent}>
         <h1 className={styles.mainTitle}>
-          <span>Лонская Диана</span>
-          <span>Владимировна</span>
+          <span>Психоаналитический терапевт и психолог</span>
           <span>Индивидуальное консультирование и психоаналитическая терапия взрослых</span>
         </h1>
-        <p className={styles.quote}>“Помогаю быть, а не стать.”</p>
+        <p className={styles.quote}>Помогаю исследовать внутренние тупики, разбираться с чувствами, осознавать конфликты, искать смыслы и осуществлять выбор.</p>
         <OpenModal
           openModal={openModal}
           name='Записаться'
