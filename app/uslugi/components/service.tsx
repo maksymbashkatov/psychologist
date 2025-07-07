5f1a38379ae136e8298e6661ee903faaab2bd333
maksymbashkatov <maksymbashkatov@gmail.com>
2025-07-07 13:32:38 +0300

    feat: add first version of services separate page

---
+++ app/uslugi/components/service.tsx
@@ -0,0 +1,27 @@
+import styles from './service.module.css';
+
+export default function Service(
+  { src, alt, title, subTitle, content, conditions }:
+  { src: string, alt: string, title: string, subTitle: string, content: Array<string>, conditions: Array<string[]> }
+) {
+  return <div className={styles.service}>
+    <img
+      src={src}
+      alt={alt}
+    />
+    <div className={styles.card}>
+      <h3 className={styles.title}>
+        {title}
+        <span className={styles.subTitle}>{subTitle}</span>
+      </h3>
+      <div className={styles.content}>
+        {content.map((p, i) =>
+          <p key={i}>{p}</p>
+        )}
+      </div>
+      {conditions.map((с, i) =>
+        <p key={i} className={styles.condition}><span>{с[0]}</span> {с[1]}</p>
+      )}
+    </div>
+  </div>
+}
---
+++ app/uslugi/page.tsx
@@ -0,0 +1,8 @@
+import Services from './components/services';
+
+
+export default function Page() {
+  return <main>
+    <Services/>
+  </main>;
+}
