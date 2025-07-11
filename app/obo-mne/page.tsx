35a20c9be80b8e7cc55ba7fbabf4077fcdb996f3
maksymbashkatov <maksymbashkatov@gmail.com>
2025-07-11 23:28:55 +0300

    refactor: add sitemap; add meta data for obo-mne and uslugi pages

--- app/obo-mne/page.tsx
+++ app/obo-mne/page.tsx
@@ -3,6 +3,16 @@ import Propositions from './components/propositions';
 import Education from './components/education';
 import Diploms from './components/diploms';
 
+export function generateMetadata() {
+  return {
+    title: 'Лонская Диана Владимировна - практикующий психолог 13+ лет.',
+    description: 'Психолог поможет вам изменить привычный взгляд на себя, увидеть скрытые мотивы вашего выбора и придаст вашей жизни больше осмысленности и удовлетворенности.',
+    alternates: {
+      canonical: `https://dianalonskaya.com/obo-mne`,
+    }
+  };
+}
+
 export default function Page() {
   return <main>
     <Hero/>
--- app/uslugi/page.tsx
+++ app/uslugi/page.tsx
@@ -1,5 +1,14 @@
 import Services from './components/services';
 
+export function generateMetadata() {
+  return {
+    title: 'Услуги предоставляемые психологом Лонской Дианой.',
+    description: 'Индивидуальная и групповая психотерапия, супервизии и консультации для личностного роста, преодоления тревоги и укрепления эмоционального благополучия.',
+    alternates: {
+      canonical: `https://dianalonskaya.com/uslugi`,
+    }
+  };
+}
 
 export default function Page() {
   return <main>
